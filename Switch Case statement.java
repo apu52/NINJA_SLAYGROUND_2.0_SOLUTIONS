20f8f4a4a3042f5c32cb0ae6d0a7ba6bcaa9ce26
Arpan Chowdhury <arpanchowdhury003@gmail.com>
2024-11-21 12:12:56 +0530

    Update Switch Case statement.java

--- Switch Case statement.java
+++ Switch Case statement.java
@@ -1,17 +1,17 @@
 public class Solution {
     public static double areaSwitchCase(int ch, double []a) {
         switch (ch) {
-            case 1: // Area of a circle
-                double r = a[0]; // Radius of the circle
-                return Math.PI * r * r; // Return area of the circle
+            case 1: 
+                double r = a[0]; 
+                return Math.PI * r * r; 
 
-            case 2: // Area of a rectangle
-                double l = a[0]; // Length of the rectangle
-                double b = a[1]; // Breadth of the rectangle
-                return l * b; // Return area of the rectangle
+            case 2: 
+                double l = a[0]; 
+                double b = a[1]; 
+                return l * b; 
 
             default:
-                return 0.0; // In case of invalid choice
+                return 0.0; 
         }
     }
 }
