2e61e5e32e85f8cb9314b03f4503f07c294fbe7b
Arpan Chowdhury <arpanchowdhury003@gmail.com>
2024-11-21 12:12:25 +0530

    Update Nth Fibonacci Number.java

--- Nth Fibonacci Number.java
+++ Nth Fibonacci Number.java
@@ -4,23 +4,23 @@ using namespace std;
 int main()
 {
     int n;
-    cin >> n; // Input the value of 'n'
+    cin >> n; 
     
     if (n == 1 || n == 2) {
-        cout << 1 << endl; // First two Fibonacci numbers are 1
+        cout << 1 << endl; 
         return 0;
     }
     
-    // Iterative approach to calculate Fibonacci numbers
-    int prev1 = 1, prev2 = 1; // Initialize F(1) and F(2)
+    
+    int prev1 = 1, prev2 = 1; 
     int curr = 0;
     
     for (int i = 3; i <= n; ++i) {
-        curr = prev1 + prev2; // F(n) = F(n-1) + F(n-2)
-        prev2 = prev1; // Update F(n-2) to previous F(n-1)
-        prev1 = curr; // Update F(n-1) to current F(n)
+        curr = prev1 + prev2; 
+        prev2 = prev1; 
+        prev1 = curr; 
     }
     
-    cout << curr << endl; // Output the n-th Fibonacci number
+    cout << curr << endl; 
     return 0;  
 }
