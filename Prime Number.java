0ed2567b35914523fe582ee4b75bb512c4ad45c0
Arpan Chowdhury <arpanchowdhury003@gmail.com>
2024-11-21 12:11:57 +0530

    Update Prime Number.java

--- Prime Number.java
+++ Prime Number.java
@@ -1,16 +1,16 @@
 public class Solution {
     public static boolean isPrime(int n) {
-        // If n is less than 2, it's not a prime number
+        
         if (n < 2) {
             return false;
         }
-        // Check divisors from 2 to the square root of n
+        
         for (int i = 2; i <= Math.sqrt(n); i++) {
             if (n % i == 0) {
-                return false; // n is divisible by i, so it's not prime
+                return false; 
             }
         }
-        // If no divisors were found, n is prime
+       
         return true;
     }
 }
